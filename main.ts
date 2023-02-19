7375ad28f4d2e01924b89bda0e24d407ef62fff8
magggieyang171 <116053282+magggieyang171@users.noreply.github.com>
2023-02-19 10:33:11 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(0)
+    Count = 0
+})
+input.onButtonPressed(Button.B, function () {
+    Count += 1
+    basic.showNumber(Count)
+})
+let Count = 0
+basic.showNumber(0)
+Count = 0
+basic.forever(function () {
+	
+})
